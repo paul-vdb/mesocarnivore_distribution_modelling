86346f759ff125a7158734303376a963b5b6534c
Hurtado <Cindy.Hurtado@gov.bc.ca>
2024-02-08 16:22:47 -0800

    Simulation errors Ydim[1]

---
+++ MDM_simulations/1. Determine priors.R
@@ -0,0 +1,78 @@
+# Burgar's et al. 2019 SCR analysis ####
+
+# Priors ###
+
+# 1. from Chandler and royle 2013 supplementary material ####
+
+# PART 4
+# Commented R code describing method used to obtain
+# informative prior used in the analysis of the parula dataset.
+# Moldenhauer and Regelski (1996) state that home range size typically
+# ranges from 0.08-0.65 ha, which equates to a radius of
+
+sqrt(0.08/pi*10000) # 15.96 m or
+sqrt(0.65/pi*10000) # 45.49 m
+
+# Since parulas were detected by song, let's be safe and add a minimum and maximum distance at which they could be heard, 50 and 250 m, based upon Simons et al. (2009).
+# So now we have an area between
+
+(15.96+50)^2*pi / 10000  # 1.37 ha, and
+(45.49+250)^2*pi / 10000 # 27.4 ha
+
+# We note that this is a very large range.
+
+# Following Royle et. al (2011), and assuming a
+# chi-squared distribution with 2 degrees of freedom,
+# the range of sigma is given by
+
+sqrt(1.37*10000/pi)/sqrt(5.99)   # 27 m
+sqrt(27.4*10000/pi)/sqrt(5.99)   # 120 m
+
+# In our grid spacing, 1 unit = 50m, so our we want a prior with most
+# of the density between:
+
+27/50  # 0.54
+121/50 # 2.42
+
+# Gamma(13, 10) covers this nicely
+#Cindy: so needs data values for gamma distribution that fit within 0.54 and 2.42?
+
+qgamma(c(0.001, 0.5, 0.999), 13, 10)
+
+plot(function(x) dgamma(x, 13, 10), 0, 5, xlim=c(0, 3), ylim=c(0, 1.5))
+
+#2.  Jo Burgars et al 2019 #### 
+
+#home range data to determine gamma values for HR varying form 4 to 72, 16km2 and 40km2 ##
+
+# For home ranges sizes between 4-72 km2, the range of σ in model units of 1 km is:
+# 5.99 is the chi square value from a significance level of 0.05 with 2 degrees of freedom. 
+
+(sqrt(4/pi)/1)/sqrt(5.99) # 0.46
+
+(sqrt(16/pi)/1)/sqrt(5.99) # 0.92
+
+(sqrt(40/pi)/1)/sqrt(5.99) # 1.46
+
+(sqrt(72/pi)/1)/sqrt(5.99) # 1.95
+
+# Resulting in prior distributions with most of the density between 0.46-1.95
+
+qgamma(c(0.05,0.5,0.95),60,60) # 0.79754 (min) 0.99445(med) 1.22139 (max) - 16 km2. Values need to be from 0.92 to 1.95
+
+qgamma(c(0.05,0.5,0.95),75,50) # 1.2269 1.4933 1.7958 - 40 km2.  Values need to be from 1.46 to 1.95
+
+qgamma(c(0.05,0.5,0.95),4,4) # 0.34158 0.91802 1.93841 - 4-72 km2. Values need to be from 0.46 to 1.95
+
+
+# 3. Columbian populations ####
+
+# the home estimated home range for each population is: 
+
+(sqrt(4/pi)/1)/sqrt(5.99) # 0.46
+
+(sqrt(16/pi)/1)/sqrt(5.99) # 0.92
+
+(sqrt(40/pi)/1)/sqrt(5.99) # 1.46
+
+(sqrt(72/pi)/1)/sqrt(5.99) # 1.95
---
+++ MDM_simulations/1. State space.R
@@ -0,0 +1,101 @@
+### Defining state space for Columbian populations ###
+
+library(terra)
+library(ggplot2)
+library(sf)
+library(readr)
+library(farver)
+library(ggplot2)
+library(rmapshaper)
+library(stars)
+#
+require(sf)
+
+#1. Read and plot layers
+
+setwd("C:/LocalR")
+populations <- sf::st_read("BC_Fisher_populations_2024.gdb", layer = "Fisher_population_units")
+barrier <- sf::st_read("BC_Fisher_populations_2024.gdb", layer = "Geographic_barrier")
+subpopulations <- sf::st_read("BC_Fisher_populations_2024.gdb", layer = "Subpopulations")
+subpop_bound <- sf::st_read("BC_Fisher_populations_2024.gdb", layer = "Subpopulation_boundaries")
+Interdigit <- sf::st_read("BC_Fisher_populations_2024.gdb", layer = "Interdigitation_zone")
+boreal <- sf::st_read("BC_Fisher_populations_2024.gdb", layer = "Boreal_population")
+columbian <- sf::st_read("BC_Fisher_populations_2024.gdb", layer = "Columbian_population")
+grid_columbian <- st_read("BC_meso_grid_columbian.shp")
+columbian_area <- st_union(grid_columbian, by_feature = FALSE)
+columbian_area <- ms_simplify(columbian_area, keep = 0.01,
+                      keep_shapes = FALSE)
+#plot
+
+bc_bound = bcmaps::bc_bound()
+regs = bcmaps::nr_regions()
+
+plot1 = ggplot() +
+  geom_sf(data = bc_bound) +
+  geom_sf(data = populations, aes(fill= Pop_unit)) +
+  geom_sf(data = barrier, color = 'black') 
+plot1
+
+
+subpop_plot = ggplot() +
+  geom_sf(data = bc_bound) +
+  geom_sf(data = subpopulations, aes(fill= Subpop))+
+  geom_sf(data = subpop_bound, color = 'black')
+subpop_plot
+
+plot3 = ggplot() +
+  geom_sf(data = bc_bound) +
+  geom_sf(data = Interdigit, color= 'yellow')
+plot3
+
+#2. simplify shape file of  subpopulations 
+
+library(rmapshaper)
+subpop <- ms_simplify(subpopulations, keep = 0.001,
+                                keep_shapes = FALSE)
+
+subpop_plot = ggplot() +
+  geom_sf(data = bc_bound) +
+  geom_sf(data = subpop, aes(fill= Subpop))+
+  geom_sf(data = subpop_bound, color = 'black')+
+  coord_sf(crs = 3005) #3005 albers
+subpop_plot
+
+#3. Remove boreal population
+
+subpop2 <- subpop |> dplyr::filter(Subpop != "Boreal")
+
+#3. convert to raster 
+
+template = rast(vect(subpop),res=3000)
+
+subpop_raster <- rasterize(vect(subpop2), field= "Subpop", template)
+plot(subpop_raster)
+
+rasterplot = ggplot() +
+  geom_spatraster(data = subpop_raster)+
+  #geom_sf(data = bc_bound, color= 'black', fill= NA) +
+  geom_sf(data = columbian_area, fill= NA)+
+  #geom_sf(data = grid_columbian, fill= NA)+
+  coord_sf(crs = 3005)  
+rasterplot 
+
+#4. Clip extent to columbian area, can also dissolve 3 subpopulations and clip extent with that 
+
+subpop_raster_cp <- crop(subpop_raster, columbian_area, mask= T)
+
+rasterplot2 = ggplot() +
+  geom_spatraster(data = subpop_raster_cp)+
+  #geom_sf(data = bc_bound, color= 'black', fill= NA) +
+  geom_sf(data = columbian_area, fill= NA)+
+  #geom_sf(data = grid_columbian, fill= NA)+
+  geom_sf(data = Camera_data_sf, color = 'lightgreen') +
+  geom_sf(data = DNA_data_sf, color= 'lightblue', alpha=0.1) +
+  geom_sf(data = MDM_academics_sf, color= 'lightgreen', alpha=0.1) +
+  geom_sf(data = Academic_sp_fisher_sf, color= 'black', alpha=0.1)+
+  geom_sf(data = CAM_sp_map_fisher_sf, color= 'blue', alpha=0.1)+
+  geom_sf(data = df_fisher_sf, color= 'pink') +
+  coord_sf(crs = 3005)  
+rasterplot2 
+
+
