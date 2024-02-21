0cf0b30620382a7d5e94ba40d84b516929f6ad9a
Hurtado <Cindy.Hurtado@gov.bc.ca>
2024-02-21 11:36:08 -0800

    closed model with fisher parameters

---
+++ MDM_simulations/3. Simulation FISHER cariboo population.R
@@ -0,0 +1,206 @@
+### Fit various models for cariboo subpopulation
+
+##########################
+### fit marginal closed model
+##########################
+
+#.libPaths()
+#.libPaths('C:/Users/CHURTADO/AppData/R') # this is a new path
+#.libPaths('C:/Users/CHURTADO/AppData/Local/R/win-library/4.3') 
+
+setwd("C:/LocalR/mesocarnivore_distribution_modelling/MDM_simulations")
+start.time <- Sys.time()
+
+library(rjags)
+library(jagsUI)
+
+M<-300
+init_simple <- function() {
+  zi <- matrix(0L, M, jdat.i$T)
+  zi[1:(20* dim(y)[1])] <- 1 # give 1's to indviduals who were detected by SCR
+  sii <- apply(y, c(1,2), sum)
+  si <- cbind(runif(M, xlims[1], xlims[2]),
+              runif(M, ylims[1], ylims[2]))
+  for(i in 1:nrow(sii)) {
+    si[i,1] <- mean(X.s[sii[i,] > 0, 1])
+    si[i,2] <- mean(X.s[sii[i,] > 0, 2])
+  }
+  list(z = zi, 
+       s = si,
+       p0.S=0.1, p0.O=0.05,
+       sigma=3)
+}
+pars <- c("N","psi","p0.S","p0.O","sigma","Never")
+
+for(i in 1:nsims){
+  name.i <- paste("dat.cariboo_", stub, "_", i, sep = "")
+  obj.i <- get(name.i)
+  out.i <- paste("out.", stub, "_", i, sep = "")
+  y <- obj.i$y.s # observed SCR data for first T
+  dim.y <- dim(y)
+  y.orig <- array(0L, c(dim.y[1] + 1, dim.y[2], dim.y[3]))
+  y.orig [1:nrow(y), , ] <-
+    y # observed data augmented only with 1 row
+  O <- obj.i$O.o[, , 1]
+  X.s <- as.matrix(obj.i$X.s)
+  X.o <- as.matrix(obj.i$X.o)
+  xlims <- obj.i$xlims
+  ylims <- obj.i$ylims
+  jdat.i <- list(
+    y.orig = y.orig,
+    n = nrow(y.orig) - 1,
+    O = O,
+    M = M,
+    #M=dim.y[1],
+    J.s = dim.y[2],
+    X.s = X.s,
+    J.o = dim(O)[[1]],
+    X.o = X.o,
+    K = dim.y[3],
+    T = 1,
+    xlims = xlims,
+    ylims = ylims
+  )
+  out <-
+    jags(
+      "margSingle_IM.JAG",
+      data = jdat.i,
+      inits = init_simple,
+      parallel = TRUE, n.cores= 18,
+      n.chains = 3,
+      n.burnin = 2000,
+      n.adapt = 100,
+      n.iter = 5000,
+      parameters.to.save = pars
+    )
+  assign(out.i, out)
+  save(list = out.i, file = paste(out.i, ".Rdata", sep = ""))
+  rm(name.i, obj.i, out.i, out)
+}
+
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time,2)
+time.taken
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# this model run and worked using data from the simulations. 
+
+##########################
+### fit marginal open model
+##########################
+setwd("C:/LocalR/mesocarnivore_distribution_modelling/MDM_simulations")
+start.time <- Sys.time()
+
+library("jagsUI")
+init_simple <- function() {
+  zi <- matrix(0L, M, jdat.i$T)
+  zi[1: (4 *(dim(y)[1])), ] <- 1 # deleted 5* in 1:5*(dim(y)[1]))
+  sii <- apply(y, c(1, 2), sum)
+  si <- cbind(runif(M, xlims[1], xlims[2]),
+              runif(M, ylims[1], ylims[2]))
+  for (i in 1:nrow(sii)) {
+    si[i, 1] <- mean(X.s[sii[i, ] > 0, 1])
+    si[i, 2] <- mean(X.s[sii[i, ] > 0, 2])
+  }
+  list(
+    z = zi,
+    s = si,
+    gamma = 0.2,
+    phi = 0.8,
+    p0.S = p0.s,
+    p0.O = p0.o,
+    sigma = 5
+  )
+}
+pars <-
+  c("N", "psi", "p0.S", "p0.O", "sigma", "Never", "gamma", "b", "phi")
+
+for (i in 1:nsims) {
+  name.i <- paste("dat.cariboo_", stub, "_", i, sep = "")
+  obj.i <- get(name.i)
+  out.i <- paste("open.cariboo_", stub, "_", i, sep = "")
+  y <- obj.i$y.s # observed SCR data for all T
+  dim.y <- dim(y)
+  y.orig <-
+    array(0L, c(dim.y[1] + 1, dim.y[2], dim.y[3], 1))
+  y.orig [1:nrow(y), , , ] <-
+    y # observed data augmented only with 1 row
+  O <- obj.i$O.o[, , ]
+  X.s <- as.matrix(obj.i$X.s)
+  X.o <- as.matrix(obj.i$X.o)
+  xlims <- obj.i$xlims
+  ylims <- obj.i$ylims
+  jdat.i <- list(
+    y.orig = y.orig,
+    n = nrow(y.orig) - 1,
+    O = O,
+    M = M,
+    J.s = dim.y[2],
+    X.s = X.s,
+    J.o = dim(O)[[1]],
+    X.o = X.o,
+    K = dim.y[3],
+    T = 1,
+    xlims = xlims,
+    ylims = ylims
+  )
+  out <-
+    jags(
+      "margSingle_IM.JAG",
+      data = jdat.i,
+      inits = init_simple,
+      parallel = TRUE, n.cores= 18,
+      n.chains = 3,
+      n.burnin = 5000,
+      n.adapt = 2000,
+      n.iter = 50000,
+      parameters.to.save = pars
+    )
+  assign(out.i, out)
+  save(list = out.i, file = paste(out.i, "50k.RData", sep = ""))
+  rm(name.i, obj.i, out.i, out)
+}
+
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time,2)
+time.taken
+
+jags.View(open.cariboo_test_IOM_1)
